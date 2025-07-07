5c05bf2dd32a7b52e4634803650e08b4bf108a70
navyumm <navyum12@gmail.com>
2025-07-07 10:22:26 +0530

    counter

--- 02-vitereact/src/main.jsx
+++ 02-vitereact/src/main.jsx
@@ -3,8 +3,21 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
- 
-    <App />
 
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank',
+//     },
+//     children: 'Gooogly DotCom'
+// }
+
+// const anotherElement = (
+//     <a href="https://google.com" target="_blank">Gooogly DotCom</a>
+// )
+
+createRoot(document.getElementById('root')).render(
+    // anotherElement 
+    App()
 )
